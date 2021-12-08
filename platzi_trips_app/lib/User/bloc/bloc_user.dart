c05bd964f9665365b3380cef533ed06a98d3cff6
N4THB3N <ncastaneda@basiscons.com>
2021-12-07 20:27:16 -0600

    Login with Google

--- platzi_trips_app/lib/User/bloc/bloc_user.dart
+++ platzi_trips_app/lib/User/bloc/bloc_user.dart
@@ -1,8 +1,20 @@
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/repository/auth_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class UserBloc implements Bloc {
+  final authRepository = AuthRepository();
+
+  // Flujo de datos - Streams
+  // Stream Firebase
+  // StreamController
+  Stream<User> streamFirebase = FirebaseAuth.instance.authStateChanges();
+  Stream<User> get authStatus => streamFirebase;
+  // Casos de uso:
+  // 1. Sign in con Google.
+
+  Future<User> signIn() => authRepository.signInFirebase();
+
   @override
-  void dispose() {
-    // TODO: implement dispose
-  }
+  void dispose() {}
 }
--- platzi_trips_app/lib/User/ui/screens/sign_in_screen.dart
+++ platzi_trips_app/lib/User/ui/screens/sign_in_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:platzi_trips_app/widgets/gradient_back.dart';
 import 'package:platzi_trips_app/widgets/button_green.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:platzi_trips_app/platzi_trips_cupertino.dart';
 
 class SignInScreen extends StatefulWidget {
   @override
@@ -10,13 +14,32 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreen extends State<SignInScreen> {
+  UserBloc userBloc;
+  AsyncSnapshot snapshot;
+  double screenwith;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return signInGoogle();
+    userBloc = BlocProvider.of(context);
+    return _handleCurrentSession();
   }
 
-  Widget signInGoogle() {
+  Widget _handleCurrentSession() {
+    return StreamBuilder(
+      stream: userBloc.authStatus,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        // snapshot - data - Object User
+
+        if (!snapshot.hasData || snapshot.hasError) {
+          return signInGoogleUI();
+        } else {
+          return PlatziTripsCupertino();
+        }
+      },
+    );
+  }
+
+  Widget signInGoogleUI() {
     return Scaffold(
       body: Stack(
         alignment: Alignment.center,
@@ -35,7 +58,9 @@ class _SignInScreen extends State<SignInScreen> {
                 ),
                 ButtonGreen(
                   text: "Login with Gmail",
-                  onPressed: () {},
+                  onPressed: () {
+                    userBloc.signIn();
+                  },
                   width: 300.0,
                   heigth: 50.0,
                 )
