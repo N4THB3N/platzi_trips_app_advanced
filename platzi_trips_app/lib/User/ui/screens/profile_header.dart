978002afa891cca16762b41e9a901f394f38beb3
N4THB3N <ncastaneda@basiscons.com>
2021-12-09 14:50:30 -0600

    Showing the profile picture from Google account

--- platzi_trips_app/lib/User/ui/screens/profile_header.dart
+++ platzi_trips_app/lib/User/ui/screens/profile_header.dart
@@ -1,39 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:platzi_trips_app/User/ui/widgets/user_info.dart';
 import 'package:platzi_trips_app/User/ui/widgets/button_bar.dart';
+import '../../bloc/bloc_user.dart';
+import '../../model/user.dart';
 
 class ProfileHeader extends StatelessWidget {
+  UserBloc userBloc;
+  User user;
+
   @override
   Widget build(BuildContext context) {
+    userBloc = BlocProvider.of<UserBloc>(context);
 
-    final title = Text(
-      'Profile',
-      style: TextStyle(
-          fontFamily: 'Lato',
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 30.0
-      ),
+    return StreamBuilder(
+      stream: userBloc.streamFirebase,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return CircularProgressIndicator();
+          case ConnectionState.none:
+            return CircularProgressIndicator();
+          case ConnectionState.active:
+            return showProfileData(snapshot);
+          case ConnectionState.done:
+            return showProfileData(snapshot);
+        }
+      },
     );
 
+    /*
     return Container(
-      margin: EdgeInsets.only(
-          left: 20.0,
-          right: 20.0,
-          top: 50.0
-      ),
+      margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0),
       child: Column(
         children: <Widget>[
           Row(
-            children: <Widget>[
-              title
-            ],
+            children: <Widget>[title],
           ),
-          UserInfo('assets/img/ann.jpg', 'Anahí Salgado','anahi@platzi.com'),
+          UserInfo('assets/img/ann.jpg', 'Anahí Salgado', 'anahi@platzi.com'),
           ButtonsBar()
         ],
       ),
-    );
+    );*/
   }
 
+  final title = Text(
+    'Profile',
+    style: TextStyle(
+        fontFamily: 'Lato',
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+        fontSize: 30.0),
+  );
+
+  Widget showProfileData(AsyncSnapshot snapshot) {
+    if (!snapshot.hasData || snapshot.hasError) {
+      print("No logueado");
+      return Container(
+        margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0),
+        child: Column(
+          children: <Widget>[
+            CircularProgressIndicator(),
+            Text('No se pudo cargar la infomación. Haz login.')
+          ],
+        ),
+      );
+    } else {
+      print("Logueado");
+      user = User(
+          name: snapshot.data.displayName,
+          email: snapshot.data.email,
+          photoURL: snapshot.data.photoURL);
+      return Container(
+        margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[title],
+            ),
+            UserInfo(user),
+            ButtonsBar()
+          ],
+        ),
+      );
+    }
+  }
 }
