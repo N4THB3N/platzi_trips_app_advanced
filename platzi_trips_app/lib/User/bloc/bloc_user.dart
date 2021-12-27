a3c1b123932fc6f60a2ce09d6682c68c9ff973f1
N4THB3N <ncastaneda@basiscons.com>
2021-12-26 20:03:12 -0600

    List of places saved

--- platzi_trips_app/lib/User/bloc/bloc_user.dart
+++ platzi_trips_app/lib/User/bloc/bloc_user.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:platzi_trips_app/Place/model/place.dart';
 import 'package:platzi_trips_app/Place/repository/firebase_storage_repository.dart';
+import 'package:platzi_trips_app/Place/ui/widgets/widgets/profile_place.dart';
 import 'package:platzi_trips_app/User/repository/auth_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:platzi_trips_app/User/repository/cloud_firestore_api.dart';
 import 'package:platzi_trips_app/User/repository/cloud_firestore_repository.dart';
 import 'package:platzi_trips_app/User/model/user.dart' as UserModel;
 
@@ -31,6 +34,15 @@ class UserBloc implements Bloc {
 
   Future<void> updatePlaceData(Place place) =>
       _cloudFirestoreRepository.updatePlaceDate(place);
+
+  Stream<QuerySnapshot> placesListStream = FirebaseFirestore.instance
+      .collection(CloudFirestoreAPI().PLACES)
+      .snapshots();
+
+  Stream<QuerySnapshot> get PlacesStream => placesListStream;
+
+  List<ProfilePlace> buildPlaces(List<DocumentSnapshot> placesListSnapshot) =>
+      _cloudFirestoreRepository.buildPlaces(placesListSnapshot);
 
   final _firebaseStorageRepository = FirebaseStorageRepository();
   Future<UploadTask> uploadFile(String path, File image) async =>
